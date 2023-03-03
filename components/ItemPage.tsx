a60f31bcfb9b7b81440648782eb4a67b053b0ea3
Brian Guterl <brianguterl99@gmail.com>
2023-03-02 20:27:11 -0500

    layout for item page created

--- components/ItemPage.tsx
+++ components/ItemPage.tsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
+import Image from "next/image";
+
+import styles from "@/styles/itemPage.module.scss";
 
 interface ItemProps {
   id: string | string[] | undefined;
 }
 interface Item {
-  ItemId: string;
+  ItemId: number;
   DeviceName: string;
   itemIcon_URL: string;
   Price: number;
+  ShortDesc: string;
+  Type: string;
+  ItemTier: number;
+  ItemDescription: {
+    Description: string;
+    SecondaryDescription: string;
+  };
 
   prevState: [];
 }
@@ -26,7 +36,41 @@ const ItemPage: React.FC<ItemProps> = ({ id }) => {
   }, []);
 
   return (
-    <div>{item ? <div>{item.DeviceName}</div> : <div>Loading...</div>}</div>
+    <div>
+      {item ? (
+        <div className={styles.itemPage}>
+          <div className={styles.itemMain}>
+            <h2>Details</h2>
+            <hr />
+            <p>{item.ItemDescription.Description}</p>
+            <p>{item.ItemDescription.SecondaryDescription}</p>
+          </div>
+          <div className={styles.itemSideCard}>
+            <h2>{item.DeviceName}</h2>
+            <Image
+              src={item.itemIcon_URL}
+              alt={`icon of ${item.DeviceName}`}
+              width={200}
+              height={200}
+            />
+            <span className={styles.itemCardBottom}>
+              <div className={styles.itemSideTitles}>
+                <p>Type</p>
+                <p>Price</p>
+                <p>Desc</p>
+              </div>
+              <div className={styles.itemSideInfo}>
+                <p>{item.Type}</p>
+                <p>{item.Price}</p>
+                <p>{item.ShortDesc}</p>
+              </div>
+            </span>
+          </div>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
   );
 };
 
