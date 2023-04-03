4cff578830d117ad91601c967a4195fa680f180e
Brian Guterl <brianguterl99@gmail.com>
2023-04-03 19:35:11 -0400

    added data to player page

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import styles from "@/styles/playerPage.module.scss";
+
 interface PlayerProps {
   name: string | string[] | undefined;
 }
@@ -50,11 +52,39 @@ const Player: React.FC<PlayerProps> = ({ name }) => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <div>{player ? player.Name : <p>Loading...</p>}</div>
-      </div>
-    </div>
+    <>
+      {player ? (
+        <div className={styles.playerPage}>
+          <div className={styles.playerAccountInfo}>
+            <h2>{player.Name}</h2>
+            <p>Region: {player.Region}</p>
+            <p>Platform: {player.Platform}</p>
+            <p>Account Created: {player.Created_Datetime}</p>
+            <p>Last Logged In: {player.Last_Login_Datetime}</p>
+            <p>Hours Played: {player.HoursPlayed}</p>
+          </div>
+          <div className={styles.playerStats}>
+            <h3>Level {player.Level}</h3>
+            <p>Mastery Level {player.MasteryLevel}</p>
+            <p>Wins: {player.Wins}</p>
+            <p>Lossed: {player.Losses}</p>
+            <p>Leaves: {player.Leaves}</p>
+            <p>Achievements: {player.Total_Achievements}</p>
+            <p>Worshippers: {player.Total_Worshippers}</p>
+          </div>
+          <div className={styles.rankedStats}>
+            <p>Ranked Conquest: {player.Rank_Stat_Conquest.toFixed(0)}</p>
+            <p>Tier Conquest: {player.Tier_Conquest}</p>
+            <p>Ranked Duel: {player.Rank_Stat_Duel.toFixed(0)}</p>
+            <p>Tier Duel: {player.Tier_Duel}</p>
+            <p>Ranked Joust: {player.Rank_Stat_Joust.toFixed(0)}</p>
+            <p>Tier Joust: {player.Tier_Joust}</p>
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
   );
 };
 
