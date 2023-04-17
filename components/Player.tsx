720254ac0daca7fd62b24dce06e569c9527faab2
Brian Guterl <brianguterl99@gmail.com>
2023-04-17 19:43:25 -0400

    added .env for serverUrl and updated getserversideprops for player page to properly update when searching new players

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,63 +1,47 @@
-import { useEffect, useState } from "react";
-
 import styles from "@/styles/playerPage.module.scss";
 
 interface PlayerProps {
-  name: string | string[] | undefined;
-}
-
-interface Player {
-  Name: string;
-  Platform: string;
-  Region: string;
-  Personal_Status_Message: string;
-  TeamId: number;
-  Team_Name: string;
-  Created_Datetime: string;
-  Last_Login_Datetime: string;
-  HoursPlayed: number;
-  MinutesPlayed: number;
-  Level: number;
-  MasteryLevel: number;
-  Wins: number;
-  Losses: number;
-  Leaves: number;
-  Total_Achievements: number;
-  Total_Worshippers: number;
-  Avatar_URL: string;
-  Rank_Stat_Conquest: number;
-  Rank_Stat_Conquest_Controller: number;
-  Rank_Stat_Duel: number;
-  Rank_Stat_Duel_Controller: number;
-  Rank_Stat_Joust: number;
-  Rank_Stat_Joust_Controller: number;
-  Tier_Conquest: number;
-  Tier_Duel: number;
-  Tier_Joust: number;
-}
-
-const serverUrl = "http://localhost:4000";
-
-const Player: React.FC<PlayerProps> = ({ name }) => {
-  const [player, setPlayer] = useState<Player>();
-
-  const playerFetch = async () => {
-    const playerResp = await fetch(`${serverUrl}/getplayer/${name}`);
-    const playerData = await playerResp.json();
-    setPlayer(playerData[0]);
+  player: {
+    Name: string;
+    Platform: string;
+    Region: string;
+    Personal_Status_Message: string;
+    TeamId: number;
+    Team_Name: string;
+    Created_Datetime: string;
+    Last_Login_Datetime: string;
+    HoursPlayed: number;
+    MinutesPlayed: number;
+    Level: number;
+    MasteryLevel: number;
+    Wins: number;
+    Losses: number;
+    Leaves: number;
+    Total_Achievements: number;
+    Total_Worshippers: number;
+    Avatar_URL: string;
+    Rank_Stat_Conquest: number;
+    Rank_Stat_Conquest_Controller: number;
+    Rank_Stat_Duel: number;
+    Rank_Stat_Duel_Controller: number;
+    Rank_Stat_Joust: number;
+    Rank_Stat_Joust_Controller: number;
+    Tier_Conquest: number;
+    Tier_Duel: number;
+    Tier_Joust: number;
   };
+}
 
-  useEffect(() => {
-    playerFetch();
-  }, []);
-
+const Player: React.FC<PlayerProps> = ({ player }) => {
   return (
     <>
       {player ? (
         <div className={styles.playerPage}>
           <div className={styles.playerAccountInfo}>
-            <h2>{player.Name}</h2>
-            <p>Region: {player.Region}</p>
+            <h2>
+              {player.Name.charAt(0).toUpperCase() + player.Name.slice(1)}
+            </h2>
+            {player.Region ? <p>Region: {player.Region}</p> : null}
             <p>Platform: {player.Platform}</p>
             <p>Account Created: {player.Created_Datetime}</p>
             <p>Last Logged In: {player.Last_Login_Datetime}</p>
--- pages/player/[Name].tsx
+++ pages/player/[Name].tsx
@@ -1,15 +1,51 @@
-import { useRouter } from "next/router";
+import { GetServerSideProps } from "next";
 
 import Player from "../../components/Player";
 
-export default function PlayerPage() {
-  const router = useRouter();
-
-  return <Player name={router.query.name} />;
-}
-
-export async function getServerSideProps() {
-  return {
-    props: {},
+type PlayerData = {
+  player: {
+    Name: string;
+    Platform: string;
+    Region: string;
+    Personal_Status_Message: string;
+    TeamId: number;
+    Team_Name: string;
+    Created_Datetime: string;
+    Last_Login_Datetime: string;
+    HoursPlayed: number;
+    MinutesPlayed: number;
+    Level: number;
+    MasteryLevel: number;
+    Wins: number;
+    Losses: number;
+    Leaves: number;
+    Total_Achievements: number;
+    Total_Worshippers: number;
+    Avatar_URL: string;
+    Rank_Stat_Conquest: number;
+    Rank_Stat_Conquest_Controller: number;
+    Rank_Stat_Duel: number;
+    Rank_Stat_Duel_Controller: number;
+    Rank_Stat_Joust: number;
+    Rank_Stat_Joust_Controller: number;
+    Tier_Conquest: number;
+    Tier_Duel: number;
+    Tier_Joust: number;
   };
+};
+
+const serverUrl = process.env.serverUrl;
+
+export default function PlayerPage(player: PlayerData["player"]) {
+  return <Player player={player} />;
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  player: PlayerData;
+}> = async ({ query }) => {
+  const playerResp = await fetch(`${serverUrl}/getplayer/${query.name}`);
+  const playerData = await playerResp.json();
+  return {
+    props: playerData[0],
+  };
+};
