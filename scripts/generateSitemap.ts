b3897b10d232d6913163f69dc20a20ebfcb7e425
Brian Guterl <brianguterl99@gmail.com>
2023-07-07 20:25:09 -0400

    added pages to ignore and set up sitemap layout for dynamic pages

--- scripts/generateSitemap.ts
+++ scripts/generateSitemap.ts
@@ -4,5 +4,28 @@ import prettier from "prettier";
 
 const generate = async () => {
   const prettierConfig = await prettier.resolveConfig("./.prettier.js");
-  const pages = await globby(["pages/*.js"]);
+  const pages = await globby([
+    "pages/*.js",
+    "!pages/player",
+    "!pages/api",
+    "!pages/404.js",
+  ]);
+
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages
+          .map((page) => {
+            const path = page.replace("pages", "").replace(".js", "");
+            const route = path === "/index" ? "" : path;
+
+            return `
+              <url>
+                  <loc>${`https://www.smitesage.com${route}`}</loc>
+              </url>
+            `;
+          })
+          .join("")}
+    </urlset>
+    `;
 };
