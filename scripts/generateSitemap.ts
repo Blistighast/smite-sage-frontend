39a5164dab369d99f1a4c5c79026443316d6e881
Brian Guterl <brianguterl99@gmail.com>
2023-07-10 21:57:58 -0400

    finished sitemap generator script function

--- scripts/generateSitemap.ts
+++ scripts/generateSitemap.ts
@@ -16,7 +16,11 @@ const generate = async () => {
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${pages
           .map((page) => {
-            const path = page.replace("pages", "").replace(".js", "");
+            const path = page
+              .replace("pages", "")
+              .replace("data", "")
+              .replace(".js", "")
+              .replace(".mdx", "");
             const route = path === "/index" ? "" : path;
 
             return `
@@ -28,4 +32,14 @@ const generate = async () => {
           .join("")}
     </urlset>
     `;
+
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: "html",
+  });
+
+  // eslint-disable-next-line no-sync
+  writeFileSync("public/sitemap.xml", formatted);
 };
+
+generate();
