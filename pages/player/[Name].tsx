06247efaef476997555c9f3b18ff19962fc4b4ba
Brian Guterl <brianguterl99@gmail.com>
2023-07-31 12:02:35 -0400

    removed unused code from player page and component

--- pages/player/[Name].tsx
+++ pages/player/[Name].tsx
@@ -5,21 +5,8 @@ import { useEffect, useState } from "react";
 
 export const dynamic = "force-dynamic"; //should fix search params bug of empty props
 
-export default function PlayerPage({ params }: { params: { name: string } }) {
-  const [queryName, setQueryName] = useState<
-    string | string[] | undefined | null
-  >("");
-
-  const searchParams = useSearchParams();
+export default function PlayerPage() {
   const pathname = usePathname();
 
-  useEffect(() => {
-    setQueryName(searchParams.get("name"));
-    console.log(pathname?.slice(8));
-  }, [searchParams]);
-
-  // return <Player name={params.name} />;
-  // return <Player name={searchParams.get("name")} />;
   return <Player name={pathname?.slice(8)} />; // slicing out /player/
-  // return <Player name={queryName} />;
 }
