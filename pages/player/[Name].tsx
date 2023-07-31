b89be106636c0203249411acebdda2d3dfeb35cd
Brian Guterl <brianguterl99@gmail.com>
2023-07-30 23:18:43 -0400

    replaced useSearchParams with usePathname for player search

--- pages/player/[Name].tsx
+++ pages/player/[Name].tsx
@@ -1,21 +1,25 @@
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 import Player from "../../components/Player";
 import { useEffect, useState } from "react";
 
 export const dynamic = "force-dynamic"; //should fix search params bug of empty props
 
-export default function PlayerPage() {
+export default function PlayerPage({ params }: { params: { name: string } }) {
   const [queryName, setQueryName] = useState<
     string | string[] | undefined | null
   >("");
 
   const searchParams = useSearchParams();
+  const pathname = usePathname();
 
   useEffect(() => {
     setQueryName(searchParams.get("name"));
+    console.log(pathname?.slice(8));
   }, [searchParams]);
 
-  return <Player name={searchParams.get("name")} />;
+  // return <Player name={params.name} />;
+  // return <Player name={searchParams.get("name")} />;
+  return <Player name={pathname?.slice(8)} />; // slicing out /player/
   // return <Player name={queryName} />;
 }
