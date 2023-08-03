9518985a814c958813ad62ec004489df35ad97d8
Brian Guterl <brianguterl99@gmail.com>
2023-08-02 21:40:39 -0400

    updated player search page to use replace instead

--- pages/player/[Name].tsx
+++ pages/player/[Name].tsx
@@ -1,12 +1,12 @@
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 import Player from "../../components/Player";
-import { useEffect, useState } from "react";
 
 export const dynamic = "force-dynamic"; //should fix search params bug of empty props
 
 export default function PlayerPage() {
   const pathname = usePathname();
 
-  return <Player name={pathname?.slice(8)} />; // slicing out /player/
+  console.log(pathname?.replace("/player/", ""));
+  return <Player name={pathname?.replace("/player/", "")} />;
 }
